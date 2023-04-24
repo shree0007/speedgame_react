54b67df7edab3b18c97584d09f1b163370237978
shree0007 <shree.bhusal07@gmail.com>
2023-04-24 20:50:26 +0300

    speed game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import Circle from './Circle';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    score: 0,
+    counter: 0,
+    circles: [1, 2, 3, 4]
+  }
+  clickHandler = () => {
+
+    this.setState({
+      score: this.state.score + 10
+    })
+
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <div className='container'>
+          <div>
+            <h1>SPEED GAME</h1>
+          </div>
+          <div>
+            <p>SCORE: <span>{this.state.score}</span></p>
+          </div>
+          <div className='circles'>
+            {this.state.circles.map(() => (<Circle click={this.clickHandler} />))}
+          </div>
+          <div className='buttons'>
+            <button>START</button>
+            <button>END</button>
+          </div>
+
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Circle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Circle = (props) => {
+    return (
+        <div className='circle' onClick={props.click}>
+
+        </div>
+    );
+};
+
+export default Circle;
